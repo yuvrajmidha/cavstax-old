0c76663a8c0a5242bc7ad9a79ea047d5bc3e12ed
RY Yuvraj <inventive8983@gmail.com>
2021-02-17 18:41:20 +0530

    HomePage

---
+++ configs/theme.js
@@ -0,0 +1,14 @@
+// 1. Import `extendTheme`
+import { extendTheme } from "@chakra-ui/react"
+// 2. Call `extendTheme` and pass your custom values
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#f7fafc",
+      // ...
+      900: "#1a202c",
+    },
+  },
+})
+
+export default theme
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import Header from '../components/header'
+import { ChakraProvider } from "@chakra-ui/react"
+import '../styles/fonts.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+  
+    </Head>
+    <ChakraProvider>
+      <Header></Header>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  </>
 }
 
 export default MyApp
